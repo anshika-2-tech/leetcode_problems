ad38b5c26fcc407a9b102327479cbb914801fbdf
anshika-2-tech <singhanshika2404@gmail.com>
2026-04-12 23:15:57 +0530

    new stack problem

---
+++ stack/next greater element I.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        nge_map = {}  
+       
+        for num in nums2:
+            while stack and num > stack[-1]:
+                smaller = stack.pop()
+                nge_map[smaller] = num
+            stack.append(num)
+        
+        
+        while stack:
+            nge_map[stack.pop()] = -1
+        
+        return [nge_map[num] for num in nums1]
